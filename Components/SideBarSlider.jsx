a0df34bb06bf263fa2d9a04a5731060d0f9311e1
mirJakub <slawomir.krzyzak@gmail.com>
2023-12-26 13:48:14 +0100

    install sass, added scss, created navbar slider productpage components, export default, import

---
+++ Components/SideBarSlider.jsx
@@ -0,0 +1,8 @@
+function SideBarSlider (){
+    return ( 
+    <>
+    </>
+    )
+}
+
+export default SideBarSlider;
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../Styles/App.scss'
+
+import NavBar from '../Components/NavBar'
+import SideBarSlider from '../Components/SideBarSlider'
+import ProductDetail from '../Pages/ProductDetail'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <NavBar/>
+    <SideBarSlider/>
+    <ProductDetail/>
+Hello there!
     </>
   )
 }
