1e350203d31b6d8c7e61b75dde225118e899c2b6
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-06 14:32:24 +0100

    hello from navbar

--- Components/SideBarSlider.jsx
+++ Components/SideBarSlider.jsx
@@ -6,7 +6,7 @@ import { Carousel } from '@mantine/carousel';
 function SideBarSlider() {
     return (
 
-        <div style={{ height: 400, display: 'flex'}}>
+        <div style={{ height: '50vh', display: 'flex', backgroundColor:'white',color:"red"}}>
             <Carousel
                 slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
                 height={200}
@@ -17,7 +17,10 @@ function SideBarSlider() {
                 dragFree
                 withControls={false}
             >
-                <Carousel.Slide>1</Carousel.Slide>
+                <Carousel.Slide>    <img 
+      src="https://github.com/sonali582000/Module2-jsonBackendServer/blob/master/img/SliderSideBar/P0.jpg"
+      alt="new"
+      /></Carousel.Slide>
                 <Carousel.Slide>2</Carousel.Slide>
                 <Carousel.Slide>3</Carousel.Slide>
             </Carousel>
