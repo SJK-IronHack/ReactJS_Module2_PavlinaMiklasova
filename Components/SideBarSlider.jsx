19b38df1d525b01ebb9b42e2ea72eee2cf32793d
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-08 21:49:14 +0100

    removed hardcoaded carousel

--- Components/SideBarSlider.jsx
+++ Components/SideBarSlider.jsx
@@ -36,9 +36,6 @@ function SideBarSlider() {
               </li>
             </>
           ))}
-          <Carousel.Slide>1</Carousel.Slide>
-          <Carousel.Slide>2</Carousel.Slide>
-          <Carousel.Slide>3</Carousel.Slide>
         </Carousel>
       </div>
     </>
