2e706286e5edb230cbdeb2ce15067b76e2494ee4
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-08 15:50:23 +0100

    just a push

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,13 @@ import '../Styles/App.modules.scss'
 import NavBar from '../Components/NavBar'
 import SideBarSlider from '../Components/SideBarSlider'
 import ProductDetail from '../Pages/ProductDetail'
-import axios from "axios";
 
 
 const API_URL = 'http://localhost:4000/products'
 
 
 function App() {
-  const [projects, setProjects] = useState();
-  const getAllProjects = () => {
-    axios
-      .get(`${API_URL}/projects?_embed=tasks`)
-      .then((response) => setProjects(response.data))
-      .catch((error) => console.log(error));
-  };
-  useEffect(() => {
-    getAllProjects();
-  }, []);
+
 
 
   return (
