3f5fe061307f4259b10d1c428f98795fce247798
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-09 21:47:39 +0100

    delete function

--- Pages/DeleteItem.jsx
+++ Pages/DeleteItem.jsx
@@ -1,27 +1,28 @@
-
-import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 const API_URL = "http://localhost:4000";
 
+function DeleteButton({ projectId, title, description, year, thumbnail, images }) {
+  const navigate = useNavigate();
 
-function DeleteButton() {
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const requestData = { title, description, year, thumbnail, images };
 
-    const handleDelete = (e) => {
-        e.preventDefault();
-        const requestData = { title, description, year, thumbnail, images };
-        axios
-            .DELETE(`${API_URL}/projects/${projectId}`, requestData)
-            .then((response) => {
-                alert("success");
-                navigate(`/`);
-            });
-    }
-    return (
-        <button type="submit" onClick={handleDelete}>Delete the Project</button>
+    axios
+      .delete(`${API_URL}/projects/${projectId}`, { data: requestData })
+      .then((response) => {
+        alert("Success");
+        navigate(`/`);
+      })
+      .catch((error) => {
+        console.error("Error deleting project:", error);
+      });
+  };
 
-    )
-
-} 
+  return <button type="submit" onClick={handleDelete}>Delete the Project</button>;
+}
 
 export default DeleteButton;
