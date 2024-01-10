5e788107e147e31a33e3a5d7d1fec294dcde935a
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-10 11:19:31 +0100

    delete item button

--- Pages/ProductDetail.jsx
+++ Pages/ProductDetail.jsx
@@ -3,6 +3,8 @@ import { AllProjectContext } from "../Contexts/AllProjectsContext";
 import { useContext, useEffect, useState } from "react";
 // import "../Styles/DetailComponentStyles.modules.scss";
 import DeleteItem from './DeleteItem'
+import AddItem from "./AddItem";
+import EditItem from "./EditItem";
 
 function ProductDetail() {
   const { projectId } = useParams();
@@ -13,12 +15,30 @@ function ProductDetail() {
     setProject(getOneProduct(projectId));
   }, [projectId]);
 
+  const handleDelete = (e) => {
+    e.preventDefault();
+    const requestData = { title, description, year, thumbnail, images };
+
+    axios
+      .delete(`${API_URL}/projects/${projectId}`, { data: requestData })
+      .then((response) => {
+        alert("Success");
+        navigate(`/`);
+      })
+      .catch((error) => {
+        console.error("Error deleting project:", error);
+      });
+  };
+
   return (
     <>
+    
       <p>{projectId}</p>
       {project && <p>{project.description}</p>}
       {project && <p>{project.title}</p>}
-      < DeleteItem/>
+      <button type="submit" onClick={handleDelete}>Delete the Project</button>
+      <AddItem/>
+      <EditItem/> 
     </>
   );
 }
