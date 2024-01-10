6556f1b93b2f436910ddd5402aa45c2d73c17a44
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-10 19:05:10 +0100

    fixed img slider

--- Components/ImagesSliderDetail.jsx
+++ Components/ImagesSliderDetail.jsx
@@ -6,21 +6,20 @@ import { useParams } from "react-router-dom";
 const ImagesSliderDetail = () => {
   const [imageData, setImageData] = useState([]);
   const { projectId } = useParams();
+  const [project, setProject] = useState();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         // Replace the URL with the actual URL hosting your JSON file
-        const response = await axios.get(`${import.meta.env.VITE_API_URL}`);
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/projects/${projectId}`);
 
         // Check if response.data.projects is an array
-
         // Extract images and thumbnails from the API data
-        const extractedData = response.data.projects.map((item) => ({
-          thumbnail: item.images.thumbnail,
-          images: item.images,
+        const extractedData = response.data.images.map((item) => ({
+          image: item.url,
         }));
-
+setProject(response.data);
         // Set the image data state with the array of objects containing thumbnails and images
         setImageData(extractedData);
       } catch (error) {
@@ -33,17 +32,23 @@ const ImagesSliderDetail = () => {
 
   return (
     <div>
-      <h2>Image Gallery</h2>
+      {/* <h2>Image Gallery</h2>
       {imageData.map((item, index) => (
         <div key={index}>
           {project && <img src={project.thumbnail} />}
           <div>
-            {Object.values(item.images).map((imageUrl, i) => (
-              <img key={i} src={imageUrl} alt={`Image ${index + 1}.${i + 1}`} />
-            ))}
+       {imageData.map((image, index) => {
+        return ( <img src={image.url} key={index}/>)
+       })}
           </div>
         </div>
-      ))}
+      ))} */}
+      {project ? (project.images.map((image, index) =>{
+        console.log(image);
+        return (
+          <img src={image.url} key={index}  alt="img not working" style={{height:"100px"}}/>
+        )
+      }) ): null}
     </div>
   );
 };
